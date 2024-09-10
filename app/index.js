5ea8720c8dd200e89601b67d991e27a2d05c0571
guto2279 <wesley55daniel@gmail.com>
2024-09-09 23:51:24 -0300

    Algumas explicações do professor

---
+++ app/index.js
@@ -0,0 +1,31 @@
+// Hello world 
+
+let mensagem = "Váriavel"
+const msg = "Constante"
+console.log(mensagem);
+console.log(msg);
+
+
+// arrays
+
+let array1 = ["guto", "Wesley"]
+
+console.log(array1[0] + ", " + array1[1])
+
+//objeto
+
+let meta = {
+    value: 'Ler um livro todo mês',
+    checked: false
+} //chaves significa um objeto
+
+console.log(meta.value)
+
+
+//function // arrow Function
+
+const criarMeta = () => {
+
+}
+
+//function criarMeta(){}  outro estilo de função

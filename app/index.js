24646e781fe7f97fd193a5779238a26fc47c426d
guto2279 <wesley55daniel@gmail.com>
2024-09-11 22:48:17 -0300

    cadastrar metas

--- app/index.js
+++ app/index.js
@@ -1,4 +1,17 @@
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
+
+let metas = []
+
+const cadastrarMeta = async () => {
+    const meta = await input ({message: "Digite a meta: "})
+    
+    if (meta.length == 0){
+        console.log("A meta não pode ser vazia")
+        return
+    }
+
+    metas.push({ value:meta, checked: false})
+}
 
 const start = async() => { 
     while(true){
@@ -24,6 +37,8 @@ const start = async() => {
         switch(opcao){
             case "cadastrar":
                 console.log("Vamos cadastrar")
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
                 console.log("Vamos Listar")
